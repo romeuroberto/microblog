146c8cdbb7e14ce814751a3b6e9e7c34efa055f7
romeuroberto <robertoromeu366@gmail.com>
2022-01-23 14:18:21 -0300

    first commit

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('587') or 25)
+    MAIL_USE_TLS = os.environ.get('1') is not None
+    MAIL_USERNAME = os.environ.get('robertoromeu366@gmail.com')
+    MAIL_PASSWORD = os.environ.get('sherlock13#thrones')
+    ADMINS = ['robertoromeu366@gmail.com']
+    POSTS_PER_PAGE = 3
