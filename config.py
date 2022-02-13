e6b5040df8167838ecbc688eb564c9ccf7e06fba
romeuroberto <robertoromeu366@gmail.com>
2022-02-13 18:15:49 -0300

    Commit final do Tutorial

--- config.py
+++ config.py
@@ -1,16 +1,25 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('smtp.googlemail.com')
-    MAIL_PORT = int(os.environ.get('587') or 25)
-    MAIL_USE_TLS = os.environ.get('1') is not None
-    MAIL_USERNAME = os.environ.get('robertoromeu366@gmail.com')
-    MAIL_PASSWORD = os.environ.get('sherlock13#thrones')
-    ADMINS = ['robertoromeu366@gmail.com']
-    POSTS_PER_PAGE = 3
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
+    LANGUAGES = ['en', 'es']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'
+    POSTS_PER_PAGE = 25
